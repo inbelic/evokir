3aedbce66e621ff5ba2f11c2c17d3245f8bbe014
inbelic <canadienfinn@gmail.com>
2023-10-18 21:43:57 +0200

    fixup

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -12,7 +12,7 @@ config.suffixes = [".mlir"]
 
 # lit executes relative to the directory
 #
-#   bazel-bin/tests/<test_target_name>.runfiles/mlir_tutorial/
+#   bazel-bin/tests/<test_target_name>.runfiles/evokir/
 #
 # which contains all the binary targets included in via the `data` attribute in
 # the lit.bzl macro, which in turn gets them from the filegroup //tests:test_utilities.
@@ -25,9 +25,9 @@ config.suffixes = [".mlir"]
 #   print(subprocess.run(["ls", "-l", os.environ["RUNFILES_DIR"]]).stdout)
 #   print(subprocess.run([ "env", ]).stdout)
 #
-# Bazel defines RUNFILES_DIR which includes mlir_tutorial/ and third party
+# Bazel defines RUNFILES_DIR which includes evokir/ and third party
 # dependencies as their own directory. Generally, it seems that $PWD ==
-# $RUNFILES_DIR/mlir_tutorial/
+# $RUNFILES_DIR/evokir/
 runfiles_dir = Path(os.environ["RUNFILES_DIR"])
 tool_relpaths = [
     "llvm-project/mlir",
